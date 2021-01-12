0f43779d6f89e6f49e7407ea320a397affaa7d1e
CodersTokyo <info@coders.tokyo>
2021-01-12 16:05:12 +0800

    Update

--- client/src/const/const.js
+++ client/src/const/const.js
@@ -1,3 +1,1 @@
-export const link = 'http://localhost:9081'
-
-export const link_old = 'https://pet-project-renthouse.herokuapp.com'
+export const link = 'https://pet-project-renthouse.herokuapp.com'
