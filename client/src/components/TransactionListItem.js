de9140c54e4e4068a6d6faec42589712e5664795
CodersTokyo <info@coders.tokyo>
2021-01-14 10:29:15 +0800

    update

--- client/src/components/TransactionListItem.js
+++ client/src/components/TransactionListItem.js
@@ -43,7 +43,8 @@ const TransactionListItem = ({ transaction }) => {
         {
           content: comment,
           billId: bill._id
-        }
+        },
+        {headers: {Authorization: token}}
       );
       setTransactions(res.data);
       setComment("");
